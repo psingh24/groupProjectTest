152abacc05d681ed08ec160efca511af68aee5c8
Prabhdeep <ps591990@gmail.com>
2017-06-03 22:24:19 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,171 +1,171 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-//  // Initialize Firebase
-//   var config = {
-//     apiKey: "AIzaSyDbb4OYFm4EDFMLBPUSvSib8xQ38x6CVKE",
-//     authDomain: "project-app-43963.firebaseapp.com",
-//     databaseURL: "https://project-app-43963.firebaseio.com",
-//     projectId: "project-app-43963",
-//     storageBucket: "project-app-43963.appspot.com",
-//     messagingSenderId: "416945318872"
-//   };
-//   firebase.initializeApp(config);
+ // Initialize Firebase
+   var config = {
+    apiKey: "AIzaSyCgQFFxv6-cd0vRQesrZUD447sO7AEYklo",
+    authDomain: "clickevent-e44d0.firebaseapp.com",
+    databaseURL: "https://clickevent-e44d0.firebaseio.com",
+    projectId: "clickevent-e44d0",
+    storageBucket: "clickevent-e44d0.appspot.com",
+    messagingSenderId: "673662977249"
+  };
+  firebase.initializeApp(config);
 
-// var database = firebase.database();
-// var ref = database.ref("preferences");
-// var foodArray = [];
-// var drinksArray = [];
-// var eventsArray = [];
+var database = firebase.database();
+var ref = database.ref("preferences");
+var foodArray = [];
+var drinksArray = [];
+var eventsArray = [];
 
-// $(".drinks-section").hide();
-// $(".events-section").hide();
+$(".drinks-section").hide();
+$(".events-section").hide();
 
-// $("#drinks-question").on("click", function(){
-// 	$(".drinks-section").show();
-// 	$(".food-section").hide();
-// 	$(".events-section").hide();
-// 	$(".section-title").html("<h2> Select the type of drinks(s) you are thirsty for: </h2> <p style='font-size: 16px;''>Select up to two (2) drinks only.</p>");
-// });
+$("#drinks-question").on("click", function(){
+	$(".drinks-section").show();
+	$(".food-section").hide();
+	$(".events-section").hide();
+	$(".section-title").html("<h2> Select the type of drinks(s) you are thirsty for: </h2> <p style='font-size: 16px;''>Select up to two (2) drinks only.</p>");
+});
 
-// $("#events-question").on("click", function(){
-// 	$(".events-section").show();
-// 	$(".food-section").hide();
-// 	$(".drinks-section").hide();
-// 	$(".section-title").html("<h2> Select the type of event(s) you are interested in: </h2>");
-// });
+$("#events-question").on("click", function(){
+	$(".events-section").show();
+	$(".food-section").hide();
+	$(".drinks-section").hide();
+	$(".section-title").html("<h2> Select the type of event(s) you are interested in: </h2>");
+});
 
-// $("#food-question").on("click", function(){
-// 	$(".food-section").show();
-// 	$(".events-section").hide();
-// 	$(".drinks-section").hide();
-// 	$(".section-title").html("<h2> Select the type of food(s) you are craving: </h2>");
-// });
+$("#food-question").on("click", function(){
+	$(".food-section").show();
+	$(".events-section").hide();
+	$(".drinks-section").hide();
+	$(".section-title").html("<h2> Select the type of food(s) you are craving: </h2>");
+});
 
-// function food() {
-// 	$(".food").on("click", function(){
-// 		if($(this).attr("data-state")=="unclicked"){
-// 			var selection = $(this).attr("data-name");
-// 			foodArray.push(selection);
-// 			if($(this).attr("data-type")!="caption"){
-// 				$(this).addClass("clicked");
-// 				$("#caption"+$(this).attr("data-name")).attr("data-state","clicked");
-// 			}
-// 			else if($(this).attr("data-type")=="caption"){
-// 				$("#"+$(this).attr("data-name")).addClass("clicked");
-// 				$("#"+$(this).attr("data-name")).attr("data-state","clicked");
-// 			}
-// 			$(this).attr("data-state","clicked");
-// 		}
-// 		else if ($(this).attr("data-state")=="clicked"){
-// 			var selection = $(this).attr("data-name");
-// 			var index = foodArray.indexOf(selection);
-// 			foodArray.splice(index, 1);
+function food() {
+	$(".food").on("click", function(){
+		if($(this).attr("data-state")=="unclicked"){
+			var selection = $(this).attr("data-name");
+			foodArray.push(selection);
+			if($(this).attr("data-type")!="caption"){
+				$(this).addClass("clicked");
+				$("#caption"+$(this).attr("data-name")).attr("data-state","clicked");
+			}
+			else if($(this).attr("data-type")=="caption"){
+				$("#"+$(this).attr("data-name")).addClass("clicked");
+				$("#"+$(this).attr("data-name")).attr("data-state","clicked");
+			}
+			$(this).attr("data-state","clicked");
+		}
+		else if ($(this).attr("data-state")=="clicked"){
+			var selection = $(this).attr("data-name");
+			var index = foodArray.indexOf(selection);
+			foodArray.splice(index, 1);
 
-// 			if($(this).attr("data-type")!="caption"){
-// 				$(this).removeClass("clicked");
-// 				$("#caption"+$(this).attr("data-name")).attr("data-state","unclicked");
-// 			}
-// 			else if($(this).attr("data-type")=="caption"){
-// 				$("#"+$(this).attr("data-name")).removeClass("clicked");
-// 				$("#"+$(this).attr("data-name")).attr("data-state","unclicked");
-// 			}
-// 			$(this).attr("data-state","unclicked");
-// 		}
-// 		console.log(foodArray);
+			if($(this).attr("data-type")!="caption"){
+				$(this).removeClass("clicked");
+				$("#caption"+$(this).attr("data-name")).attr("data-state","unclicked");
+			}
+			else if($(this).attr("data-type")=="caption"){
+				$("#"+$(this).attr("data-name")).removeClass("clicked");
+				$("#"+$(this).attr("data-name")).attr("data-state","unclicked");
+			}
+			$(this).attr("data-state","unclicked");
+		}
+		console.log(foodArray);
 
-// 	});
-// }
+	});
+}
 
-// function drinks() {
-// 	$(".drinks").on("click", function(){
-// 		if($(this).attr("data-state")=="unclicked"){
-// 			if(drinksArray.length<2){
-// 				var selection = $(this).attr("data-name");
-// 				drinksArray.push(selection);
+function drinks() {
+	$(".drinks").on("click", function(){
+		if($(this).attr("data-state")=="unclicked"){
+			if(drinksArray.length<2){
+				var selection = $(this).attr("data-name");
+				drinksArray.push(selection);
 
-// 				if($(this).attr("data-type")!="caption"){
-// 					$(this).addClass("clicked");
-// 					$("#caption"+$(this).attr("data-name")).attr("data-state","clicked");
-// 				}
-// 				else if($(this).attr("data-type")=="caption"){
-// 					$("#"+$(this).attr("data-name")).addClass("clicked");
-// 					$("#"+$(this).attr("data-name")).attr("data-state","clicked");
-// 				}
-// 				$(this).attr("data-state","clicked");
-// 			}
-// 		}
-// 		else if ($(this).attr("data-state")=="clicked"){
-// 			var selection = $(this).attr("data-name");
-// 			var index = drinksArray.indexOf(selection);
-// 			drinksArray.splice(index, 1);
+				if($(this).attr("data-type")!="caption"){
+					$(this).addClass("clicked");
+					$("#caption"+$(this).attr("data-name")).attr("data-state","clicked");
+				}
+				else if($(this).attr("data-type")=="caption"){
+					$("#"+$(this).attr("data-name")).addClass("clicked");
+					$("#"+$(this).attr("data-name")).attr("data-state","clicked");
+				}
+				$(this).attr("data-state","clicked");
+			}
+		}
+		else if ($(this).attr("data-state")=="clicked"){
+			var selection = $(this).attr("data-name");
+			var index = drinksArray.indexOf(selection);
+			drinksArray.splice(index, 1);
 			
-// 			if($(this).attr("data-type")!="caption"){
-// 				$(this).removeClass("clicked");
-// 				$("#caption"+$(this).attr("data-name")).attr("data-state","unclicked");
-// 			}
-// 			else if($(this).attr("data-type")=="caption"){
-// 				$("#"+$(this).attr("data-name")).removeClass("clicked");
-// 				$("#"+$(this).attr("data-name")).attr("data-state","unclicked");
-// 			}
-// 			$(this).attr("data-state","unclicked");
-// 		}
-// 	    console.log(drinksArray);
-// 	});
-// }
+			if($(this).attr("data-type")!="caption"){
+				$(this).removeClass("clicked");
+				$("#caption"+$(this).attr("data-name")).attr("data-state","unclicked");
+			}
+			else if($(this).attr("data-type")=="caption"){
+				$("#"+$(this).attr("data-name")).removeClass("clicked");
+				$("#"+$(this).attr("data-name")).attr("data-state","unclicked");
+			}
+			$(this).attr("data-state","unclicked");
+		}
+	    console.log(drinksArray);
+	});
+}
 
-// function events() {
-// 	$(".events").on("click", function(){
-// 		if($(this).attr("data-state")=="unclicked"){
-// 			var selection = $(this).attr("data-name");
-// 			eventsArray.push(selection);
+function events() {
+	$(".events").on("click", function(){
+		if($(this).attr("data-state")=="unclicked"){
+			var selection = $(this).attr("data-name");
+			eventsArray.push(selection);
 
-// 			if($(this).attr("data-type")!="caption"){
-// 				$(this).addClass("clicked");
-// 				$("#caption"+$(this).attr("data-name")).attr("data-state","clicked");
-// 			}
-// 			else if($(this).attr("data-type")=="caption"){
-// 				$("#"+$(this).attr("data-name")).addClass("clicked");
-// 				$("#"+$(this).attr("data-name")).attr("data-state","clicked");
-// 			}
-// 			$(this).attr("data-state","clicked");
-// 		}
-// 		else if ($(this).attr("data-state")=="clicked"){
-// 			var selection = $(this).attr("data-name");
-// 			var index = eventsArray.indexOf(selection);
-// 			eventsArray.splice(index, 1);
+			if($(this).attr("data-type")!="caption"){
+				$(this).addClass("clicked");
+				$("#caption"+$(this).attr("data-name")).attr("data-state","clicked");
+			}
+			else if($(this).attr("data-type")=="caption"){
+				$("#"+$(this).attr("data-name")).addClass("clicked");
+				$("#"+$(this).attr("data-name")).attr("data-state","clicked");
+			}
+			$(this).attr("data-state","clicked");
+		}
+		else if ($(this).attr("data-state")=="clicked"){
+			var selection = $(this).attr("data-name");
+			var index = eventsArray.indexOf(selection);
+			eventsArray.splice(index, 1);
 			
-// 			if($(this).attr("data-type")!="caption"){
-// 				$(this).removeClass("clicked");
-// 				$("#caption"+$(this).attr("data-name")).attr("data-state","unclicked");
-// 			}
-// 			else if($(this).attr("data-type")=="caption"){
-// 				$("#"+$(this).attr("data-name")).removeClass("clicked");
-// 				$("#"+$(this).attr("data-name")).attr("data-state","unclicked");
-// 			}
-// 			$(this).attr("data-state","unclicked");
-// 		}
-// 	    console.log(eventsArray);
-// 	});
-// }
+			if($(this).attr("data-type")!="caption"){
+				$(this).removeClass("clicked");
+				$("#caption"+$(this).attr("data-name")).attr("data-state","unclicked");
+			}
+			else if($(this).attr("data-type")=="caption"){
+				$("#"+$(this).attr("data-name")).removeClass("clicked");
+				$("#"+$(this).attr("data-name")).attr("data-state","unclicked");
+			}
+			$(this).attr("data-state","unclicked");
+		}
+	    console.log(eventsArray);
+	});
+}
 
-// food();
-// drinks();
-// events();
+food();
+drinks();
+events();
 
-// function submit(){
-// 	$("#submitBtn").on("click", function(event){
-// 		event.preventDefault();
-// 	    console.log(eventsArray);
-// 	    console.log(foodArray);
-// 	    console.log(drinksArray);
+function submit(){
+	$("#submitBtn").on("click", function(event){
+		event.preventDefault();
+	    console.log(eventsArray);
+	    console.log(foodArray);
+	    console.log(drinksArray);
 
-// 	   	ref.update({
-// 	   		food : foodArray,
-// 	   		drinks : drinksArray,
-// 	   		events : eventsArray
-// 	   	});	   
-// 	});
-// }
+	   	ref.update({
+	   		food : foodArray,
+	   		drinks : drinksArray,
+	   		events : eventsArray
+	   	});	   
+	});
+}
 
-// submit();
-// });
+submit();
+});
