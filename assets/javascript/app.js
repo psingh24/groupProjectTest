dadabab891c0e1de97a99945677858b2c83defc7
Prabhdeep <ps591990@gmail.com>
2017-06-04 19:10:01 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -92,7 +92,7 @@ firebase.auth().onAuthStateChanged(function(firebaseUser){
 	if(firebaseUser) {
        //USer is signed in
 		userHtml.html("Welcome "+ firebaseUser.displayName) 
-        ref.push({
+        ref.update({
             name: firebaseUser.displayName,
             email: firebaseUser.email,
             likes: "blank"
