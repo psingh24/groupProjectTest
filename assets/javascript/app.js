3f6e6664e9e4b4259236faa6e9ac6fd727b9c514
Prabhdeep <ps591990@gmail.com>
2017-06-06 23:22:43 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -134,6 +134,7 @@ firebase.auth().onAuthStateChanged(function(firebaseUser){
 	   username = firebaseUser.displayName
 	   JSONreadyArray = JSON.stringify(username);
 	   localStorage.setItem("name", JSONreadyArray);
+	   $("#usernameSuggestions").html(JSON.parse(localStorage['name'])+"'s Suggestions" )
 		userHtml.html("Welcome "+ firebaseUser.displayName)
 		signedIn = ref.child(firebaseUser.displayName) 
         signedIn.update({
@@ -366,7 +367,7 @@ function submit(){
 
 		
 
-	$("#usernameSuggestions").html(JSON.parse(localStorage['name'])+"'s Suggestions" )
+	
 
 
 // displayNameLocalStorage()
