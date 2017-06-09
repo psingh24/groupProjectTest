f68ffcc8cb6f56c096f5790092398baaa2bcde21
Prabhdeep <ps591990@gmail.com>
2017-06-09 00:02:27 -0500

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -356,7 +356,7 @@ function submit(){
 
 
 database.ref(username).on('value', function(snapshot) {
-  console.log(snapshot.val().food.val());
+  console.log(snapshot.val().food);
   console.log(snapshot.val().food[0]);
   console.log(snapshot.val().food[2]);
 });
